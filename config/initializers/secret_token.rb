aed5fdfb95773610ba0b67444f6e597382e14596
viceversus <apprentice@devbootcamp.com>
2012-08-01 10:06:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basecamp::Application.config.secret_token = 'b01f4aa60c38b90865953d99c3c0c01ca750e235741e6c6f47465273ba70bcc56a552730eb18217fd12ac121c4a52cfa1361e51895a532b6955d9514edf31804'
